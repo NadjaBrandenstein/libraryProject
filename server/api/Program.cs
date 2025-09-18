f0138436e227aec6f0e44b21a79c818549198851
NadjaBrandenstein <nadbra01@easv365.dk>
2025-09-19 01:38:18 +0200

    gitignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,30 @@
+using efscaffold;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql();
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+{
+    var myBooks = new Book()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Title = "test",
+        Pages = 150
+    };
+    dbContext.Books.Add(myBooks);
+    dbContext.SaveChanges();
+    
+    var books = dbContext.Books.ToList();
+    return books;
+    
+});
 
 app.Run();
