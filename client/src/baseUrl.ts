3d67ac8d85dbaca6fab1cafa252f034183f70251
NadjaBrandenstein <nadbra01@easv365.dk>
2025-09-19 23:03:02 +0200

    deployment of client + enviroment swapping

---
+++ client/src/baseUrl.ts
@@ -0,0 +1,6 @@
+﻿const isProduction = import.meta.env.prod;
+
+const prod = "https://library-project-server.fly.dev"
+const dev = "http://localhost:5173/"
+
+export const finalUrl = isProduction ? prod: dev;
