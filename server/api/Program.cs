c7b8ee5c48d9169f3c896ccfa120d769005d2c8f
NadjaBrandenstein <nadbra01@easv365.dk>
2025-09-19 02:38:46 +0200

    Deployment for server

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,17 +1,23 @@
+using api;
 using efscaffold;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    conf.UseNpgsql();
+    conf.UseNpgsql(appOptions.DbConnectionString);
 });
 
 var app = builder.Build();
 
-app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+app.MapGet("/", (
+    [FromServices] IOptions<AppOptions> options,
+    [FromServices] MyDbContext dbContext) =>
 {
     var myBooks = new Book()
     {
